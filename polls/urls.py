2c17feafbdc7aa1f6e01968e4adb891f0116d4d7
seven613 <seven613@126.com>
2020-04-09 17:09:22 +0800

    获取对象抛出异常、使用模版、命名空间

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from .  import views
 
+
+app_name='polls' #命名空间
+
 urlpatterns =  [
     
     path('',views.index,name='index'),
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 from django.template import loader
 
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 
 from .models import Question
@@ -26,7 +26,20 @@ def index(request):
 
     
 def detail(request,question_id):
-    return HttpResponse("问题:%s 页面" % question_id)
+    # return HttpResponse("问题:%s 页面" % question_id)
+    """
+    1.查找问题，不存在就抛异常
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("不存在的问题") # 抛出异常
+    """
+    """
+    2.使用快捷键get_object_or_404
+    """
+    question = get_object_or_404(Question,pk=question_id)
+
+    return render(request,'polls/detail.html',{'question':question})
 
 def results(request,question_id):
     return HttpResponse("问题:%s的投票结果页" % question_id)
