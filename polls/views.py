9428d4daa56da6d7e3006ac53b63f60f23288bdb
seven613 <seven613@126.com>
2020-04-09 16:46:37 +0800

    定义视图页，无实际内容

--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,11 @@ def index(request):
     return HttpResponse("主页")
 
     
+def detail(request,question_id):
+    return HttpResponse("问题:%s 页面" % question_id)
+
+def results(request,question_id):
+    return HttpResponse("问题:%s的投票结果页" % question_id)
+
+def vote(request,question_id):
+    return HttpResponse("问题：%s 的投票数" % question_id)
