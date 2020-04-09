84d352c7ac17d58217e55ea221e4bbcca6d8797d
张强 <seven613@zhangqiangdeMacBook-Pro.local>
2020-04-09 20:58:32 +0800

    改正错误

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.template import loader
 
-from django.shortcuts import render,get_object_or_404
 
+from django.shortcuts import render,get_object_or_404
+from django.urls import reverse
 
 from .models import Question
 
@@ -51,6 +52,7 @@ def vote(request,question_id):
     question = get_object_or_404(Question,pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        print(selected_choice)
     except (KeyError,Choice.DoesNotExist):
         return render(request,'polls/detail.html',{
             'question':question,
