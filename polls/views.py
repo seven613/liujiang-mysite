16c9435a7f365b8093c7e799007f208112891212
seven613 <seven613@126.com>
2020-04-09 17:22:05 +0800

    完善投票流程

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse,Http404
+from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.template import loader
 
 from django.shortcuts import render,get_object_or_404
@@ -42,7 +42,21 @@ def detail(request,question_id):
     return render(request,'polls/detail.html',{'question':question})
 
 def results(request,question_id):
-    return HttpResponse("问题:%s的投票结果页" % question_id)
+    # return HttpResponse("问题:%s的投票结果页" % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{'question':question})
 
 def vote(request,question_id):
-    return HttpResponse("问题：%s 的投票数" % question_id)
+    # return HttpResponse("问题：%s 的投票数" % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        return render(request,'polls/detail.html',{
+            'question':question,
+            'error_message':'没有选择'
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
